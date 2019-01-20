082d6a39ba4c3feadf6654281f68f58f75a04833
admin <747563037@qq.com>
2019-01-20 22:10:25 +0800

    elasticsearch_day02tiajia

--- elasticasearch_day02/src/test/java/com/itheima/test/TestElasticSearchCRUD.java
+++ elasticasearch_day02/src/test/java/com/itheima/test/TestElasticSearchCRUD.java
@@ -56,6 +56,56 @@ public class TestElasticSearchCRUD {
 
 
 
+    @Test
+//创建映射
+    public void test3() throws Exception{
+        // 创建Client连接对象
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+
+        //创建名称为blog2的索引
+        client.admin().indices().prepareCreate("blog2").get();
+        // 添加映射
+        /**
+          * 格式：
+          * "mappings" : {
+              "article" : {
+                   "dynamic" : "false",
+                   "properties" : {
+                      "id" : { "type" : "string" },
+                      "content" : { "type" : "string" },
+                      "author" : { "type" : "string" }
+                   }
+                }
+            }
+          */
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("article")
+                .startObject("properties")
+                .startObject("id")
+                .field("type","long")
+                .endObject()
+                .startObject("title")
+                .field("type","string")
+                .field("store","true")
+                .field("analyzer","ik_smart")
+                .endObject()
+                .startObject("content")
+                .field("type","string")
+                .field("store","true")
+                .field("analyzer","ik_smart")
+                .endObject()
+                .endObject()
+                .endObject()
+                .endObject();
+        // 创建映射
+        PutMappingRequest mapping = Requests.putMappingRequest("blog2").type("article").source(builder);
+        client.admin().indices().putMapping(mapping).get();
+        //释放资源
+        client.close();
+    }
+
 
 
 }
