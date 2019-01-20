8e71641070897d304715b23937fe2741f189533c
admin <747563037@qq.com>
2019-01-20 21:46:10 +0800

    elasticsearch_day02修改了

--- elasticasearch_day02/src/test/java/com/itheima/test/TestElasticSearchCRUD.java
+++ elasticasearch_day02/src/test/java/com/itheima/test/TestElasticSearchCRUD.java
@@ -56,45 +56,6 @@ public class TestElasticSearchCRUD {
 
 
 
-    @Test
-    public void fun03() throws IOException, ExecutionException, InterruptedException {
-
-        Settings settings = Settings.EMPTY;
-
-        TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(
-
-                new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300)
-        );
-//创建名称为blog2的索引
-        client.admin().indices().prepareCreate("blog3").get();
-
-
-        XContentBuilder builder = XContentFactory.jsonBuilder()
-                .startObject()
-                .startObject("article")
-                .startObject("properties")
-                .startObject("id")
-                .field("type","long")
-                .endObject()
-                .startObject("title")
-                .field("type","string")
-                .field("store","true")
-                .field("analyzer","ik_smart")
-                .endObject()
-                .startObject("content")
-                .field("type","string")
-                .field("store","true")
-                .field("analyzer","ik_smart")
-                .endObject()
-                .endObject()
-                .endObject()
-                .endObject();
-        // 创建映射
-        PutMappingRequest mapping = Requests.putMappingRequest("blog2").type("article").source(builder);
-        client.admin().indices().putMapping(mapping).get();
-        //释放资源
-        client.close();
-    }
 
 
 }
